cb4fada4ef046c949006cc83266488b1d588436d
fgrhsQrStuqmVcIyUxMAiaifvDmEwntqtxhJtnf <fgrhsQrStuqmVcIyUxMAiaifvDmEwntqtxhJtnf@protonmail.com>
2018-01-10 14:01:47 -0600

    Added support for repeating list of comments.

--- reddit_tools/auto_reply/__main__.py
+++ reddit_tools/auto_reply/__main__.py
@@ -2,20 +2,25 @@ import click
 import time
 from reddit_tools.utils import get_client
 import random
+import itertools
 
 
 @click.command()
 @click.argument('reply-user')
 @click.argument('reply-content')
 def auto_reply(reply_user, reply_content):
-    print('Using reply "{}"'.format(reply_content))
+    reply_content = reply_content.split(':') * 9999
 
     while True:
-        print('Searching recent comments for user "{}".'.format(reply_user))
+        print(f'Searching recent comments for user "{reply_user}".')
         for comment in get_client().inbox.unread():
             if str(comment.author) == str(reply_user):
+                reply = reply_content.pop()
+
                 print('\tFound comment, replying.')
-                comment.reply(reply_content)
+                print(f'Using reply "{reply}"')
+
+                comment.reply(reply)
                 comment.mark_read()
                 if 5 == random.choice(range(0,10,1)):
                     comment.downvote()
