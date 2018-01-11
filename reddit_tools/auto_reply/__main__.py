beecc74e9d1d67979cbc3b4e91406bdaf9a2e82d
fgrhsQrStuqmVcIyUxMAiaifvDmEwntqtxhJtnf <fgrhsQrStuqmVcIyUxMAiaifvDmEwntqtxhJtnf@protonmail.com>
2018-01-10 23:56:49 -0600

    Removed logging unless something happens.

--- reddit_tools/auto_reply/__main__.py
+++ reddit_tools/auto_reply/__main__.py
@@ -3,6 +3,7 @@ import time
 from reddit_tools.utils import get_client
 import random
 import itertools
+import string
 
 
 @click.command()
@@ -12,19 +13,17 @@ def auto_reply(reply_user, reply_content):
     reply_content = reply_content.split(':') * 9999
 
     while True:
-        print(f'Searching recent comments for user "{reply_user}".')
         for comment in get_client().inbox.unread():
             if str(comment.author) == str(reply_user):
                 reply = reply_content.pop()
 
                 print('\tFound comment, replying.')
-                print(f'Using reply "{reply}"')
+                print(f'\tUsing reply "{reply}"')
 
                 comment.reply(reply)
                 comment.mark_read()
                 if 5 == random.choice(range(0,10,1)):
                     comment.downvote()
-        print('Sleeping for 10 seconds.')
         time.sleep(10)
 
 
